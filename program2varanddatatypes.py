068363a288ccc7e47a2423694654acd4f9c789da
Dawn S Chacko <dawnschacko03@gmail.com>
2026-03-02 23:28:06 +0530

    Dictionary in python

--- program2varanddatatypes.py
+++ program2varanddatatypes.py
@@ -32,3 +32,13 @@ list1=[1,2,3,4,5,6,7,3.145]
 list2=[8,9,10,11,12,13,14]
 print(list1+list2)
 print(float(list1[7]+list2[6]))
+
+#Tuple in python
+tuple1=(1,2,3,4,5,6)
+tuple2=(7,8,9,0,5.678)
+print(tuple1+tuple2)
+print(float(tuple1[5]+tuple2[2]))
+
+#Dictionary in python
+dict1={"name":"dawn","age":"23","gender":"male"}
+print(dict1['name'],dict1['age'])
