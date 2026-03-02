2d5d8685655194b336f4cca6dd725c60ec02de6f
Dawn S Chacko <dawnschacko03@gmail.com>
2026-03-02 14:44:19 +0530

    Python Programming - Week 1

---
+++ program1.py
@@ -0,0 +1,10 @@
+print("Hello World")
+print("hello","world", sep="\n")
+print(3+5-2)
+a= int(input("Enter an Interger:"))
+b=int(input("enter an Interger:"))
+c=a+b
+d=a-b
+e=a/b
+f=a*b
+print(c,d,e,f,sep="\n")
---
+++ program2varanddatatypes.py
@@ -0,0 +1,8 @@
+
+x=5
+y=4
+z=x+y
+z=z+1
+x=y
+y=z-x
+print(x,y,z,sep="\t")
