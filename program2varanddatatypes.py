285bbbb6b9aeee7c85c4d8f4039645b6694e7c57
Dawn S Chacko <dawnschacko03@gmail.com>
2026-03-02 23:14:40 +0530

    Tuple in python

--- program2varanddatatypes.py
+++ program2varanddatatypes.py
@@ -22,3 +22,13 @@ result1=float(str1)
 print(result1)
 result2=float(str2)
 print(result2)
+
+#String in python
+string="Hello World"
+print(string)
+
+#List in python
+list1=[1,2,3,4,5,6,7,3.145]
+list2=[8,9,10,11,12,13,14]
+print(list1+list2)
+print(float(list1[7]+list2[6]))
