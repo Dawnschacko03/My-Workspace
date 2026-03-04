61f2b7956eab8b7c625841ac309b6a16cea49735
Dawn S Chacko <dawnschacko03@gmail.com>
2026-03-04 15:48:30 +0530

    string and Float

--- program3expressions.py
+++ program3expressions.py
@@ -1,8 +1,20 @@
 #type and Id
+#integer
 x=5
 print(id(x))
 print(type(x))
 
+#Complex
 y=4+5j
 print(id(y))
 print(type(y))
+
+#String
+z="Hello my Child"
+print(id(z))
+print(type(z))
+
+#float
+l=5.6789
+print(id(l))
+print(type(l))
