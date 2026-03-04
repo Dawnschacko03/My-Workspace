af177a68e940593829fd0eb39ebeb7021fece296
Dawn S Chacko <dawnschacko03@gmail.com>
2026-03-04 16:08:48 +0530

    Shorthand or Augmented Operators

--- program4operators.py
+++ program4operators.py
@@ -8,9 +8,24 @@ print("5/2=", 5/2) #Division
 print("5//2=", 5//2) #Integer Division
 print("5%2=", 5%2) #Modulus
 
-#Logical Operators
+#Relational Operators
 print("2>5=", 2>5)
 print("2<5=", 2<5)
 print("5!=6=", 5!=6)
 print("5==5=", 5==5)
 print("4>=4.567 =", 4>=4.567)
+
+#Shorthand or AUgmented Assignment Operators
+x=5
+x+=5
+print(x)
+x-=8
+print(x)
+x**=2
+print(x)
+x/=3
+print(x)
+x*=3
+print(x)
+x//=3
+print(x)
