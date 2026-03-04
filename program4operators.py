69611778f00ba8dc8415c630259bd1874d1e8753
Dawn S Chacko <dawnschacko03@gmail.com>
2026-03-04 15:54:35 +0530

    Operators Arithmetic Operators

---
+++ program4operators.py
@@ -0,0 +1,10 @@
+#OPERATORS
+
+#Arithmetic Operators
+print("2+3=", 2+3)
+print("4*5=", 4*5)
+print("2**4=", 2**4)
+print("5/2=", 5/2) #Division
+print("5//2=", 5//2) #Integer Division
+print("5%2=", 5%2) #Modulus
+
