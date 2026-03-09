61c6a355dab56163188897f91ae7a8046cab72c6
Dawn S Chacko <dawnschacko03@gmail.com>
2026-03-09 23:29:32 +0530

    Program to check whether the given number is even or odd.

--- program13.py
+++ program13.py
@@ -1,2 +1,9 @@
-
-n=int(input(""))
+#Check whether the given number is even or odd.
+n=float(input(""))
+def numbers(n):
+    if n!=0 and n%2==0:
+        print("Even")
+    else:
+        print("Odd")
+numbers(n)
+    
