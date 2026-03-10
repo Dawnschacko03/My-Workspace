5f49b03657856b27338c98d2b07372d9000b6ecd
Dawn S Chacko <dawnschacko03@gmail.com>
2026-03-10 23:13:28 +0530

    program to find the sum of series s=(1)+(1+2)+(1+2+3)+...+(1+2+3+...+n)

--- program16.py
+++ program16.py
@@ -1,13 +1,11 @@
 #program to find sum of series s=(1)+(1+2)+(1+2+3)+...+(1+2+3+...+n)
-n=int(input(""))
-def series(n):
-    s=0
-    s1=0
-    for i in range(1,n+1):
-        s+=i
-        #s1=s+i
-        #print(s)
-        for j in range(1,n+1):
-            s1=s+j
-    print(s1)
-series(n)
+n = int(input(""))
+total = 0
+for i in range(1, n + 1):
+    temp_sum = 0
+    
+    for j in range(1, i + 1):
+        temp_sum += j
+        
+    total += temp_sum
+print("Sum of the series =", total)
