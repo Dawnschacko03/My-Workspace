f2bd02aa861eacef81b20e065fc772a45a56f848
Dawn S Chacko <dawnschacko03@gmail.com>
2026-03-20 19:55:56 +0530

    program to find the largest and smallest in thre list

--- program39.py
+++ program39.py
@@ -1,2 +1,14 @@
+#program to find the largest and smallest element in the list
+numbers=[3,4,5]
 
-ft
+largest = numbers[0]
+smallest = numbers[0]
+
+for num in numbers:
+    if num > largest:
+        largest = num
+    if num < smallest:
+        smallest = num
+
+print("Largest element:", largest)
+print("Smallest element:", smallest)
