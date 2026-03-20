36827eb24d274cc1191d960dcd91ebdcaee66237
Dawn S Chacko <dawnschacko03@gmail.com>
2026-03-20 20:31:15 +0530

    lsit the odd and even and count the number

--- program40.py
+++ program40.py
@@ -1,15 +1,21 @@
-#program to print the odd and even numbers in the list
-n=[1,2,3,4,4,5,6,77,]
+#program to print the odd and even numbers in the list and count
+n=[1,2,3,4,4,5,6,77,79]
 n1=[]
 n2=[]
-def count(n):
+def sum(n):
+    m=0
+    l=0
     for i in range(0,len(n)):
         if n[i]%2==0:
             n1.append(n[i])
-            
+            m+=1
         else: 
             n2.append(n[i])
+            l+=1
         print(n[i])
+
     print(n1)
     print(n2)
-count(n)
+    print(m)
+    print(l)
+sum(n)
