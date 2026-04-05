d63818a43db0095a59993326c85d8afc53de08c2
Dawn S Chacko <dawnschacko03@gmail.com>
2026-04-05 23:50:33 +0530

    dws

--- program52.py
+++ program52.py
@@ -2,4 +2,6 @@
 #program to list the number zero on the rightmost
 n=[1,0,2,1,1,0,2,0,3,2,3,3,1]
 for i in range(0,len(n),1):
-    if
+    for j in range(0,len(n)-i-1,1):
+        if n[j]==0:
+            n[j],n[j+1]=n[j+1],n[j]
