c9efc1849a15d5a9a14a2b0bd6daf14426f067ea
Dawn S Chacko <dawnschacko03@gmail.com>
2026-04-07 20:20:55 +0530

    dshtt

--- program52.py
+++ program52.py
@@ -2,6 +2,9 @@
 #program to list the number zero on the rightmost
 n=[1,0,2,1,1,0,2,0,3,2,3,3,1]
 for i in range(0,len(n),1):
-
+    if n[i]==0:
+        n.append(0)
+        n.remove(0)
+print(n)
 
         
