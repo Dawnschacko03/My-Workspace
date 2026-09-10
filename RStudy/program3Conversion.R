421a1ee207653148a38be889c8c07d48a072100b
Dawn S Chacko <dawnschacko03@gmail.com>
2026-09-10 23:19:27 +0100

    R

--- RStudy/program3Conversion.R
+++ RStudy/program3Conversion.R
@@ -3,3 +3,12 @@ typeof(as.double(as.integer(1)))
 typeof(as.complex(as.double(1)))
 
 typeof(as.character(as.complex(1)))
+
+20> 20
+25>=20
+25+20>=20+20
+25!=22
+
+(23>24)&(24<23)
+
+(22<24)|(24>23)
