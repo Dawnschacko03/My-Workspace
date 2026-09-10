8eaae1e7be127a0b617f7c4ad4fb56221b57848f
Dawn S Chacko <dawnschacko03@gmail.com>
2026-09-10 12:31:58 +0100

    R

--- RStudy/program2Arithmetic.R
+++ RStudy/program2Arithmetic.R
@@ -1,5 +1,5 @@
 x<-10
-y<-2
+y<-5
 r<-x+y
 v<-x*y
 g<-x/y
@@ -12,7 +12,25 @@ print(f)
 print(d)
 
 sqrt(64)
+
 pi
+
 abs(-10)
+
 exp(1)
+
 log(2.718281828459045)
+
+round(233.33234,digits=3)
+round(233.33234,2)
+round(233.33234)
+round(233.33234,digits=-2)
+
+floor(3.76)
+floor(-3.76)
+ceiling(3.7)
+ceiling(-3.7)
+
+trunc(3.9)
+trunc(-3.7)
+trunc(3.3)
